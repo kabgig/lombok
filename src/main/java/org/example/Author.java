e03e519fe694083489098f99243f657d58a56df0
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-24 21:43:39 +0800

    @Getter @Setter @ToString

---
+++ src/main/java/org/example/Author.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Author {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String biography;
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author();
+        author.setLastName("Jackson");
+        System.out.println(author);
+
+    }
+}
